336fcf98c0b31386e61711eb7d490e01350a8433
Jack O'neil <39179690+JLynx69@users.noreply.github.com>
2018-05-28 20:47:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg update && pkg upgrade -y
 pkg install python2 nano
+echo ""
 echo -n -e """silahkan edit username dan password nya !!!
-\e[1;31m\t\tPERHATIAN !!!
-\e[0m
+\t\tPERHATIAN !!!
+
 Dilarang mengubah code tanpa ijin
 jika sudah mengerti silahkan lanjut"""
+echo ""
 sleep 5
 mkdir /data/data/com.termux/files/home/.terlogin
 mv bin/login.py /data/data/com.termux/files/home/.terlogin
 echo 'python2 /data/data/com.termux/files/home/.terlogin/login.py' >> /data/data/com.termux/files/usr/etc/bash.bashrc
 nano /data/data/com.termux/files/home/.terlogin/login.py
-rm -rf terlogin
+
+echo ""
 echo -e "\n## Thanks For Using Terlogin ##"
 echo -e "silahkan mulai ulang termux\n"
+echo ""
 sleep 10
